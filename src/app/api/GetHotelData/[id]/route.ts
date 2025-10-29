6aa31abcf3cc6b4711d5b53e62e586bd9b4e227a
vendetta106 <vendetta10668@gmail.com>
2025-10-30 00:06:55 +0100

    fix(database) fix error with database, buckets, images and addHotel form

--- src/app/api/GetHotelData/[id]/route.ts
+++ src/app/api/GetHotelData/[id]/route.ts
@@ -1,27 +1,26 @@
 import { getHotel } from "@/lib/actions";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, context: any) {
-  const { params } = context;
-  const id = params.id;
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
 
   if (!id) {
     return NextResponse.json({ error: "Missing hotel ID" }, { status: 400 });
   }
 
   const hotelId = Number(id);
-
   if (isNaN(hotelId)) {
     return NextResponse.json({ error: "Invalid hotel ID" }, { status: 400 });
   }
 
   try {
     const hotel = await getHotel(hotelId);
-
     if (!hotel) {
       return NextResponse.json({ error: "Hotel not found" }, { status: 404 });
     }
-
     return NextResponse.json(hotel);
   } catch (error) {
     console.error("Error during API request:", error);
